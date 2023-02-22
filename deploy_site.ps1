f1266879ec41aa1f876df691b59f855ada45dd90
Noel Miller <noelmiller@protonmail.com>
2023-02-22 15:57:33 -0600

    deploy site 2023-02-22 15:57

--- deploy_site.ps1
+++ deploy_site.ps1
@@ -3,6 +3,6 @@ $current_date = Get-Date -Format "yyyy-MM-dd HH:mm"
 Remove-Item -Recurse .\docs
 pelican content -o .\docs
 Copy-Item .\CNAME .\docs
-git add .\docs
+git add .
 git commit -m "deploy site $current_date"
 git push origin main
